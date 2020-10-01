fc60a8af9e9442aca92d51160238cbcbe2c64d9c
Turlybek Zhuranov <turlybekk@gmail.com>
2020-10-01 07:42:11 +0600

    feat: find min, max, avg values of array

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
 exports.min = function min (array) {
+  if (array !== undefined && array.length) {
+    let min=array[0];
+    for (let i=1; i<array.length; i++){
+        if (array[i] < min){
+          min = array[i];
+        }
+    }
+    return min;
+  }
   return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array !== undefined && array.length){
+      let max=array[0];
+      for (let i=1; i<array.length; i++){
+        if (array[i] > max){
+          max = array[i];
+        }
+      }
+    return max;
+  } 
+    return 0;
+  
 }
 
 exports.avg = function avg (array) {
+  if (array !== undefined && array.length) {
+    let sum=0;
+    for (let i=0; i<array.length; i++){
+        sum+= array[i];
+    }
+  
+   
+    return sum/array.length;
+  } 
   return 0;
 }
